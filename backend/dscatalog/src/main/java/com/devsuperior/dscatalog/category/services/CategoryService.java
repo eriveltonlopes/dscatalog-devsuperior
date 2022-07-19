bd06fd549e6444debffa19150dfbd8af85f4cc9a
eriveltonlopes <enascimento2144@gmail.com>
2022-07-18 21:45:29 -0400

    H2 database

---
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/category/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscatalog.category.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.category.Repositories.CategoryRepository;
+import com.devsuperior.dscatalog.category.entities.Category;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
